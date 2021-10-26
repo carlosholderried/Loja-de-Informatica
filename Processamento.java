ad39ccdff42ee386ac6321a5ba641926acb7c67d
carlosholderried <93011560+carlosholderried@users.noreply.github.com>
2021-10-26 13:11:28 -0300

    Add files via upload

---
+++ Processamento.java
@@ -0,0 +1,25 @@
+package whatever;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Processamento {
+
+	
+
+	      int[cont] ct; 	
+	      
+	      	      	      	    	      
+	      for(int i=0;i<cont;i++){	
+	    	  						ct[i] = 0; // contador de vezes que o espaco em branco ocorre na frase
+	    	  						int n = items[i].length(); // tamanho da cadeia
+									for (int j=0; j<n; j++) {
+																if (items[i].charAt(j) == ' ') {
+																								 ct[i] = ct[i] + 1;
+																														}
+																						      								}
+	      
+	      System.out.println(ct[i],items[j]);      			    		
+	      																															}
+
+}
---
+++ Whatever.java
@@ -0,0 +1,11 @@
+package whatever;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Whatever {
+
+	public static void main(String[] args) {
+
+	}
+}
